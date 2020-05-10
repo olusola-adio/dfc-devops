1627d8e394193003d499b467a1c21b21d1709841
olusola-adio <olusola_adio@hotmail.com>
2020-05-10 13:55:23 +0100

    enabled test

--- Tests/quality/Q002.PSScripts.Quality.Tests.ps1
+++ Tests/quality/Q002.PSScripts.Quality.Tests.ps1
@@ -18,7 +18,7 @@ Describe "Code quality tests" -Tag "Quality" {
     foreach ($Script in $Scripts) {
         Context $Script.BaseName {
             forEach ($Rule in $Rules) {
-                It "Should pass Script Analyzer rule $Rule" -Skip {
+                It "Should pass Script Analyzer rule $Rule" {
                     $Result = Invoke-ScriptAnalyzer -Path $Script.FullName -IncludeRule $Rule -ExcludeRule $ExcludeRules
                     $Result.Count | Should Be 0
                 }
